0a91fee92b14eac502259935a5d240e916cc78a0
kusuma <kusuma.g@thundersoft.com>
2022-08-05 18:55:53 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,44 @@
+#variable
+'''variable:memory location to store values.
+           .should be starts with "alphabets"
+           .starts with "underscore".
+           .case sensitive" '''
+x=10
+y=30
+z=x+y
+print(x+y)
+print(z)
+
+x,y,z="thundersoft","microchip","wipro"
+print(x,y,z)
+print(x)
+print(y)
+print(z)
+
+
+'''x,y,z="tata project"
+print(x)
+print(y)
+print(z)'''
+
+x="thundersoft"
+print("i am in" +x)
+
+x="python"
+y="training"
+print(x + y)
+
+#pascal:Each eord starts with capital letter
+#camel:all are starts with capitel letter except first word
+
+Name=["HCL","capgemini","TCS","Tech"]
+#x,y,z="Tech mahendhra"
+#x,y,z=Name
+print(Name)
+print(Name)
+print(Name)
+'''print(x)
+print(y)
+print(z)'''
+
+
